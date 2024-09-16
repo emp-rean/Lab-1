837452992e8177bd85189931f64456d0fb7c3c31
emp-rean <spforimport@gmail.com>
2024-09-16 15:43:24 +0200

    Add files via upload

--- hw.cpp
+++ hw.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 
+using std::endl;
+using std::cout;
+
 int main()
 {
-	std::cout << "float | +/- 3.4E-38 -- 3.4E+38 | 32 bits" << std::endl;
+	cout << "float | +/- 3.4E-38 -- 3.4E+38 | 32 bits" << endl;
 	float f = 1.6; 
-	std::cout << f << std::endl;
-	std::cout << "double | +/- 1.7E-308 -- 1.7E+308 | 64 bits" << std::endl;
+	cout << f << endl;
+	cout << "double | +/- 1.7E-308 -- 1.7E+308 | 64 bits" << endl;
 	double d = 1.61231; 
-	std::cout << d << std::endl;
-	std::cout << "char | -128 -- 127, or 0 -- 255 | 8 bits" << std::endl;
+	cout << d << endl;
+	cout << "char | -128 -- 127, or 0 -- 255 | 8 bits" << endl;
 	char c = 101; 
-	std::cout << c << std::endl;
-	std::cout << "int | -32768 -- 32767 (if 2 bytes) or -2 147 483 648 -- 2 147 483 647 (if 4 bytes) | from 2 to 4 bytes" << std::endl;
+	cout << c << endl;
+	cout << "int | -32768 -- 32767 (if 2 bytes) or -2 147 483 648 -- 2 147 483 647 (if 4 bytes) | from 2 to 4 bytes" << endl;
 	int i = 54; 
-	std::cout << i << std::endl;
-	std::cout << "short | -32768 -- 32767 | 16 bits" << std::endl;
+	cout << i << endl;
+	cout << "short | -32768 -- 32767 | 16 bits" << endl;
 	short s = 30000; 
-	std::cout << s << std::endl;
-	std::cout << "long | -2 147 483 648 -- 2 147 483 647 | 32 bits" << std::endl;
+	cout << s << endl;
+	cout << "long | -2 147 483 648 -- 2 147 483 647 | 32 bits" << endl;
 	long l = 12876232; 
-	std::cout << l << std::endl;
-	std::cout << "long long | -9 223 372 036 854 775 808 -- 9 223 372 036 854 775 807| 64 bits" << std::endl;
+	cout << l << endl;
+	cout << "long long | -9 223 372 036 854 775 808 -- 9 223 372 036 854 775 807| 64 bits" << endl;
 	long long L = 543534756375; 
-	std::cout << L << std::endl;
-	std::cout << "bool | 2 | 1 bit " << std::endl;
+	cout << L << endl;
+	cout << "bool | 2 | 1 bit " << endl;
 	bool b = {false};
-	std::cout << b << std::endl;
-	
+	cout << b << endl;	
 }
